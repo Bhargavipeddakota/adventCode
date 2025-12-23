8666f1c2da3da826dc14f3eb9d70ff83e5ea1ac8
Peddakota Bhargavi <peddakotabhargavi@Peddakotas-MacBook-Pro.local>
2025-12-23 23:51:08 +0530

    make some generic functions

--- adventCode2019/amplifier/part1.js
+++ adventCode2019/amplifier/part1.js
@@ -11,87 +11,52 @@ const convertInput = (input) => {
 
 const parseInstruction = (instruction) => {
   const str = [...`${instruction}`.padStart(5, 0)];
-  return [str.slice(0, 3), str[3] + str[4]]
+  const opcode = +(str[3] + str[4]);
+  const modes = [str.slice(0, 3)]
     .flat()
-    .map((x) => parseInt(x));
+    .map((x) => parseInt(x)).reverse();
+  return { opcode, modes };
 };
-
-// const handleMode = (instruction, ip, memory, offset) => {
-//   return instruction[instruction.length - offset]
-//     ? ip + (offset - 1)
-//     : memory[ip + (offset - 1)];
-// };
 
 const getParam = (memory, ip, offset, mode) => {
   const param = memory[ip + offset];
   return mode === 1 ? param : memory[param];
 };
 
-const binaryOp = (fn) => (memory, ip, modes) => {
+const binaryOp = (operation) => (memory, ip, modes) => {
   const a = getParam(memory, ip, 1, modes[0]);
   const b = getParam(memory, ip, 2, modes[1]);
   const dest = memory[ip + 3];
 
-  memory[dest] = fn(a, b);
+  memory[dest] = operation(a, b);
   return ip + 4;
 };
 
-const print = (memory,ip,instruction) => {
-  const index = handleMode(instruction, ip, memory, 2);
-  console.log(memory[index]);
+const jumpOp = (predicate) => (memory, ip, modes) => {
+  const arg1 = getParam(memory, ip, 1, modes[0]);
+  const arg2 = getParam(memory, ip, 2, modes[1]);
+
+  return predicate(arg1) ? arg2 : ip + 3;
+};
+
+const print = (memory, ip, modes) => {
+  const value = getParam(memory, ip, 1, modes[0]);
+  console.log(value);
   return ip + 2;
 };
-const takeInput = (memory,index,instruction) => {
-  memory[memory[index + 1]] = +prompt("Enter input:");
-  return index + 2
-};
-const add = (memory, ip, instruction) => {
-  const arg1 = handleMode(instruction, ip, memory, 2);
-  const arg2 = handleMode(instruction, ip, memory, 3);
-  const result = handleMode(instruction, ip, memory, 4);
-  memory[result] = memory[arg1] + memory[arg2];
-  return ip + 4;
-};
-const mul = (memory, ip,instruction) => {
-  const arg1 = handleMode(instruction, ip, memory, 2);
-  const arg2 = handleMode(instruction, ip, memory, 3);
-  const result = handleMode(instruction, ip, memory, 4);
-  memory[result] = memory[arg1] * memory[arg2];
-  return ip + 4;
-};
-const jumpIfTrue = (memory, ip,instruction) => {
-  const p1 = handleMode(instruction, ip, memory, 2);
-  const p2 = handleMode(instruction, ip, memory, 3);
 
-  if (memory[p1] !== 0) return memory[p2];
-  return ip + 3;
+const takeInput = (memory, ip) => {
+  const dest = memory[ip + 1];
+  memory[dest] = +prompt("Enter input:");
+  return ip + 2;
 };
 
-const jumpIfFalse = (memory,ip,instruction ) => {
-  const p1 = handleMode(instruction, ip, memory, 2);
-  const p2 = handleMode(instruction, ip, memory, 3);
-
-  if (memory[p1] === 0) return memory[p2];
-  return ip + 3;
-};
-
-const lessThan = (memory,ip,instruction) => {
-  const p1 = handleMode(instruction, ip, memory, 2);
-  const p2 = handleMode(instruction, ip, memory, 3);
-  const dest  = memory[ip + 3];
-
-  memory[dest] = memory[p1] < memory[p2] ? 1 : 0;
-  return ip + 4;
-};
-
-const equalsOp = (memory,ip,instruction) => {
-  const p1 = handleMode(instruction, ip, memory, 2);
-  const p2 = handleMode(instruction, ip, memory, 3);
-  const dest = memory[ip + 3];
-
-  memory[dest] = memory[p1] === memory[p2] ? 1 : 0;
-  return ip + 4;
-};
+const add = binaryOp((a, b) => a + b);
+const mul = binaryOp((a, b) => a * b);
+const jumpIfTrue = jumpOp((x) => x !== 0);
+const jumpIfFalse = jumpOp((x) => x === 0);
+const lessThan = binaryOp((a, b) => (a < b ? 1 : 0));
+const equalsOp = binaryOp((a, b) => (a === b ? 1 : 0));
 
 const opcodes = {
   1: add,
@@ -104,17 +69,17 @@ const opcodes = {
   8: equalsOp,
 };
 
-const excute = (memory, opcode, index) => {
-  const op = opcodes[opcode[opcode.length-1]];
-  return op(memory,index,opcode);
-}
+const execute = (memory, opcode, ip, modes) => {
+  const op = opcodes[opcode];
+  return op(memory, ip, modes);
+};
 
 const performInstruction = (memory) => {
   let ip = 0;
-  while (memory[ip] !== 99 && ip < memory.length) {
-    const oppcode = memory[ip];
-    const instruction = parseInstruction(oppcode);
-    ip = excute(memory, instruction, ip);
+
+  while (memory[ip] !== 99) {
+    const { opcode, modes } = parseInstruction(memory[ip]);
+    ip = execute(memory, opcode, ip, modes);
   }
 };
 
@@ -122,6 +87,7 @@ const main = (input) => {
   const memory = convertInput(input);
   performInstruction(memory);
 };
-// const input = "3,0,4,0,99"
-main(Deno.readTextFileSync("../intcode/input1.txt"));
+// const input = "3,0,4,0,99";
+// main(input);
 
+main(Deno.readTextFileSync("../intcode/input1.txt"));
