b39b50b20d055efcd0755124df339d2a5b97cc0a
Peddakota Bhargavi <peddakotabhargavi@Peddakotas-MacBook-Pro.local>
2025-12-28 20:31:20 +0530

    advent of code 2019 day8  part1

---
+++ adventCode2019/imageFormator/part1.js
@@ -0,0 +1,33 @@
+import { chunk } from "jsr:@std/collections";
+
+const parseInput = (input) => [...input].map(Number);
+
+const convertToLayers = (imageData, width, height) =>
+  chunk(imageData, width * height);
+
+const countZeros = (layer) =>
+  layer.filter(pix => pix === 0).length;
+
+const countOnesAndTwos = (layer) => {
+  const ones = layer.filter(p => p === 1).length;
+  const twos = layer.filter(p => p === 2).length;
+  return [ones, twos];
+};
+
+const findLayerWithFewestZeros = (layers) =>
+  layers.reduce((fewest, layer) =>
+    countZeros(layer) < countZeros(fewest) ? layer : fewest
+  );
+
+const main = () => {
+  const input = readTextFileSync("./input1.txt");
+  const imageData = parseInput(input);
+  const layers = convertToLayers(imageData, 25, 6);
+
+  const fewestZerosLayer = findLayerWithFewestZeros(layers);
+  const [ones, twos] = countOnesAndTwos(fewestZerosLayer);
+
+  console.log(ones * twos);
+};
+
+main();
